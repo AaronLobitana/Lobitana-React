f0b8e6d617e588d2ffbc933bc28dac60252c2c2a
AaronLobitana <faaron.lobitana@gmail.com>
2023-02-07 12:31:32 -0500

    MyCardV1.3
    removed weird tag.

--- src/App.js
+++ src/App.js
@@ -1,40 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
 
 function MyCard() {
   return (
-    <>
     
-    <div className='topbuttons'>
-      <button className='dupe'>Duplicate</button>
-      <button className='changebg'>Change Background</button>
-      <button className='changehg'>Change Heading</button>
-      <button className='del' id='del'>Delete</button>
-    </div>
-
-    <div className='overallcard'>
-
-      <div className='box'>
-
-      <img className='pic' src='https://pbs.twimg.com/media/FajpQj_XwAEwCWw.jpg'></img>
-
-      <div className='heading'>
-        <h1>the brim reaper</h1>
+    <div>
+      <div className='topbuttons'>
+        <button className='dupe'>Duplicate</button>
+        <button className='changebg'>Change Background</button>
+        <button className='changehg'>Change Heading</button>
+        <button className='del' id='del'>Delete</button>
       </div>
 
-      <div className='details'>
-        <h3>fear the reaper</h3>
-      </div>
+      <div className='overallcard'>
 
-      <div className='reveal'>
-        <button className='detailsbutton'>Details</button>
-      </div>
+        <div className='box'>
+
+        <img className='pic' src='https://pbs.twimg.com/media/FajpQj_XwAEwCWw.jpg' alt='the brim reaper'></img>
+
+        <div className='heading'>
+          <h1>the brim reaper</h1>
+        </div>
+
+        <div className='details'>
+          <h3>fear the reaper</h3>
+        </div>
+
+        <div className='reveal'>
+          <button className='detailsbutton'>Details</button>
+        </div>
+
+        </div>
 
       </div>
+    </div>  
+    
 
-    </div>
-      
-    </>
+  
   
   );
 }
